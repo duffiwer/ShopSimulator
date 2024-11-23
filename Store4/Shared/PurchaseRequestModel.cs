3af2cee0713fe4c4313dc7704088873cc6d06f72
duffiwer <duffiwer@gmail.com>
2024-11-23 16:22:43 +0300

    добавлены сущности

---
+++ Store4/Shared/PurchaseRequestModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Store4.Shared
+{
+    public class PurchaseRequestModel
+    {
+        public string ShopCode { get; set; }
+        public List<PurchaseRequest> PurchaseRequests { get; set; }
+    }
+
+
+}
