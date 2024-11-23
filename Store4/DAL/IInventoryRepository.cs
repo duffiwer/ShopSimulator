7a59e4f5a248815d257593d41b601c63bc8c7c3b
duffiwer <duffiwer@gmail.com>
2024-11-23 16:26:52 +0300

    добавлены интерфейсы репозиториев

---
+++ Store4/DAL/IInventoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using Store4.Shared;
+
+namespace Store4.DAL
+{
+    public interface IInventoryRepository
+    {
+        Task<List<Inventory>> GetInventoryByShopAsync(string shopCode);
+        Task UpdateInventoryAsync(string shopCode, List<Inventory> items);
+        Task<List<Inventory>> GetAllInventoryAsync();
+        Task<List<Inventory>> GetInventoryByProductAsync(string productName);
+        Task<List<PurchaseOption>> GetAffordableItemsAsync(decimal budget);
+
+        Task<decimal> PurchaseItemsAsync(string shopCode, List<Inventory> purchaseRequests);
+        Task<List<Inventory>> GetInventoryByProductsAsync(string shopCode, List<string> productNames);
+        Task<IEnumerable<Inventory>> GetInventoriesAsync(); // Добавляем метод
+    }
+}
---
+++ Store4/DAL/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using Store4.Shared;
+
+namespace Store4.DAL
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllAsync();
+        Task AddProductAsync(Product product);
+    }
+}
